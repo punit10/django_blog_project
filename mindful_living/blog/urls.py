07c1e944eb2b359699934f5fb44906c33fb05d3c
punit sharna <punitcs0067@gmail.com>
2024-06-03 14:15:19 +0530

    Added CRUD for the blog post, basic strct WIP!

--- mindful_living/blog/urls.py
+++ mindful_living/blog/urls.py
@@ -1,7 +1,10 @@
-from django.urls import path
+from django.urls import path, re_path
 from blog import views
 
 urlpatterns = [
+    path('', views.PostListView.as_view(), name='post_list'),
     path('about/', views.AboutView.as_view(), name='about_view'),
-
+    re_path('post/(?P<pk>\d+)', views.PostDetailView.as_view(), name='post_detail'),
+    path('post/new/', views.CreatePostView.as_view(), name='post_new'),
+    re_path('post/(?P<pk>\d+)/edit/', views.UpdatePostView.as_view(), name='post_edit')
 ]
--- mindful_living/blog/views.py
+++ mindful_living/blog/views.py
@@ -1,8 +1,43 @@
 from django.shortcuts import render
-from django.views.generic import (TemplateView)
+from django.views.generic import (TemplateView, ListView,
+                                  DetailView, CreateView, UpdateView)
+from blog.models import Post, Comment
+from blog.forms import PostForm, CommentForm
+from django.utils import timezone
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 # Create your views here.
 
 
 class AboutView(TemplateView):
     template_name = 'about.html'
+
+
+class PostListView(ListView):
+    model = Post
+
+    def get_queryset(self):
+        return Post.objects.filter(published_date__lte=timezone.now().order_by('-published_date'))
+
+
+class PostDetailView(DetailView):
+    model = Post
+
+
+"""login_required can be done using decorators in template based views but here 
+we are using LoginRequiredMixin in class based views, for similar task.
+"""
+
+
+class CreatePostView(LoginRequiredMixin, CreateView):
+    login_url = '/login/'
+    redirect_field_name = 'blog/post_detail.html'
+    form_class = PostForm
+    model = Post
+
+
+class UpdatePostView(LoginRequiredMixin, CreateView):
+    login_url = '/login/'
+    redirect_field_name = 'blog/post_detail.html'
+    form_class = PostForm
+    model = Post
