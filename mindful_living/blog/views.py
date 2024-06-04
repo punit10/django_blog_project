0a740ed302c607a46c9cd8ebe6a7ecf03522b15b
punit sharna <punitcs0067@gmail.com>
2024-06-04 22:23:09 +0530

    Added add comment to post and other html files

--- mindful_living/blog/views.py
+++ mindful_living/blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
 from django.views.generic import (TemplateView, ListView,
                                   DetailView, CreateView,
                                   UpdateView, DeleteView)
@@ -7,6 +7,7 @@ from blog.forms import PostForm, CommentForm
 from django.urls import reverse_lazy
 from django.utils import timezone
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 
@@ -57,3 +58,22 @@ class DraftListView(LoginRequiredMixin, UpdateView):
 
     def get_queryset(self):
         return Post.objects.filter(published_date__isnull=True).order_by('-published_date')
+
+
+# Comments section #####################################
+########################################################
+
+@login_required()
+def add_comment_to_post(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == 'POST':
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            comment = form.save(commit = False)
+            comment.post = post
+            comment.save()
+            return redirect('post_detail', pk=post.pk)
+        else:
+            form = CommentForm()
+            return render(request, 'blog/comment_form.html', {form: form})
+
