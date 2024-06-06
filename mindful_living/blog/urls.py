ae944eec3770fba4abc5d7ded69fade497595a58
punit sharna <punitcs0067@gmail.com>
2024-06-06 16:21:38 +0530

    Added list post and details post and other pages WIP!

--- mindful_living/blog/urls.py
+++ mindful_living/blog/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path, re_path
 from blog import views
 
+app_name = 'blog'
+
 urlpatterns = [
     path('', views.PostListView.as_view(), name='post_list'),
     path('about/', views.AboutView.as_view(), name='about_view'),
